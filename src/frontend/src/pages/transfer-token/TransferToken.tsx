5ebdd4475f93cee27f6511f367ea2baf91dbf43c
Đăng Khoa <58596136+khoatvbattu@users.noreply.github.com>
2022-08-27 20:21:55 +0700

    Home + Transfer Token init

--- src/frontend/src/pages/transfer-token/TransferToken.tsx
+++ src/frontend/src/pages/transfer-token/TransferToken.tsx
@@ -1,9 +1,38 @@
-import { Box, Typography } from '@mui/material'
+import { TextField, Button, Typography, Stack } from "@mui/material";
 
 export function TransferToken() {
   return (
-    <Box>
-      <Typography>Transfer token</Typography>
-    </Box>
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+      paddingTop={"2vh"}
+    >
+      <Typography fontSize={"5vh"}>Transfer Tokens</Typography>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        paddingTop={"2vh"}
+      >
+        <TextField
+          //error
+          id="outlined-error-helper-text"
+          label="Principle ID"
+          defaultValue=""
+        />
+        <TextField
+          //error
+          id="outlined-error-helper-text"
+          label="Token"
+          defaultValue=""
+        />
+      </Stack>
+      <Button>
+        <Typography>Submit</Typography>
+      </Button>
+    </Stack>
   )
 }
